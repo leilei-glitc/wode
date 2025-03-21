7942cd5c190f07b8efcec4acfa59b3882d0609f7
leixiansheng001 <leixiansheng001@gmail.com>
2025-03-21 21:34:43 +0800

    修改 publicPath 配置

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/wode/'
-    : '/',
+  publicPath: '/wode/',
   outputDir: 'dist',
   assetsDir: 'static',
   productionSourceMap: false,
